72b00d03d89afcb8b62d6311ab9e55d05a04397f
marcoyimyaem <marcoyimyaem@gmail.com>
2023-12-21 06:34:18 +0000

    day 9

---
+++ AnotherClass.java
@@ -0,0 +1,6 @@
+public class AnotherClass {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.main(41);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.*;
+import java.util.Arrays;
+
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] nbnvbnvbn) {
+        Random r = new Random();
+        int[] s = new int[]{1,2,3,4,6,7};
+        Arrays.sort(s);
         System.out.println("Hello world");
+        System.out.println("random number generated is:"+r.nextInt(100));
+        main(32);
+
+    }
+    static void main(int num){
+        System.out.println(num);
     }
 }
 
