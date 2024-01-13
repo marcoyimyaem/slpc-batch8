beee1237d8f8938bdbc849ee84145f3946f97474
marcoyimyaem <marcoyimyaem@gmail.com>
2024-01-13 00:56:28 +0000

    added strings

--- chapter3/StringL.java
+++ chapter3/StringL.java
@@ -1,5 +1,7 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class StringL {
     public static void main(String[] args) {
         String s1 = "1";
@@ -9,6 +11,13 @@ public class StringL {
         System.out.println(s2);
         String name = "Marco";
         System.out.println(name.length());
-        System.out.println(name.charAt(5));
+        System.out.println(name.charAt(4));
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Word ;");
+        String palin = input.nextLine();
+        StringBuilder palinrev = new StringBuilder(palin);
+        String message = (palin.equalsIgnoreCase(palinrev.reverse().toString()))? "Palindrome": "not Palindrome";
+        System.out.println(message);
     }
 }
